bcc044894e1d08eec0d8b8727e37c34fb2baee23
Nate Spring <45617450+NateSpring@users.noreply.github.com>
2021-03-15 17:29:09 -0600

    Adding matrix visualizer

--- keymaps/nate/keymap.c
+++ keymaps/nate/keymap.c
@@ -2,7 +2,9 @@
 #include <stdio.h>
 
 char wpm_str[10];
-static char current_alpha_oled = '\0';
+
+#define MATRIX_DISPLAY_X 0
+#define MATRIX_DISPLAY_Y 0
 
 
 enum sofle_layers {
@@ -13,7 +15,7 @@ enum sofle_layers {
     _ADJUST,
 };
 
-enum custom_keycodes { KC_QWERTY = SAFE_RANGE, KC_LOWER, KC_RAISE, KC_ADJUST, KC_PRVWD, KC_NXTWD, KC_LSTRT, KC_LEND, KC_DLINE, KC_GIT };
+enum custom_keycodes { KC_QWERTY = SAFE_RANGE, KC_LOWER, KC_RAISE, KC_ADJUST, KC_PRVWD, KC_NXTWD, KC_LSTRT, KC_LEND, KC_DLINE, KC_GIT, KC_HOT_REL };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
@@ -31,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *            |      |      |      |      |/       /         \      \ |      |      |      |      |
      *            `----------------------------------'           '------''---------------------------'
      */
-     [_QWERTY]= LAYOUT(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_GRV, KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_MUTE, XXXXXXX, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ENT, KC_LGUI, KC_BSPC, KC_LCTRL, KC_LOWER, KC_ENT, KC_SPC, KC_RAISE, KC_RCTRL, KC_RALT, KC_RGUI),
+     [_QWERTY]= LAYOUT(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_GRV, KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_HOT_REL, XXXXXXX, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ENT, KC_LGUI, KC_BSPC, KC_LCTRL, KC_LOWER, KC_ENT, KC_SPC, KC_RAISE, KC_RCTRL, KC_RALT, KC_RGUI),
     /* LOWER
      * ,-----------------------------------------.                    ,-----------------------------------------.
      * |      |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
@@ -177,32 +179,22 @@ static void print_status_narrow(void) {
     oled_write_P(PSTR("\n\n"), false);
     // led_t led_usb_state = host_keyboard_led_state();
     // oled_write_ln_P(PSTR("CPSLK"), led_usb_state.caps_lock);
-    oled_write_P(PSTR("Key:\n"), false);
-            oled_write_P(PSTR("( "), false);
-            if (current_alpha_oled == '\0') {
-                oled_write_char('*', false);
-            } else {
-                oled_write_char(current_alpha_oled, false);
-            }
-            oled_write_P(PSTR(" )\n"), false);
-
     oled_write_P(PSTR("WPM:\n"), false);
     sprintf(wpm_str, "%03d", get_current_wpm());
     oled_write(wpm_str, false);
+}
 
-//     #define MATRIX_DISPLAY_X 0
-// #define MATRIX_DISPLAY_Y 18
-
-//   for (uint8_t x = 0; x < MATRIX_ROWS; x++) {
-//     for (uint8_t y = 0; y < MATRIX_COLS; y++) {
-//       draw_pixel(MATRIX_DISPLAY_X + y + 2, MATRIX_DISPLAY_Y + x + 2,(matrix_get_row(x) & (1 << y)) > 0, NORM);
-//     }
-//   }
+static void print_matrix(void){
+    for (uint8_t x = 0; x < MATRIX_ROWS; x++) {
+        for (uint8_t y = 0; y < MATRIX_COLS; y++) {
+            oled_write_pixel(MATRIX_DISPLAY_X + y + 2, MATRIX_DISPLAY_Y + x + 2,(matrix_get_row(x) & (1 << y))> 0);
+        }
+    }
 //   draw_rect_soft(MATRIX_DISPLAY_X, MATRIX_DISPLAY_Y, 19, 9, PIXEL_ON, NORM);
-//   /* hadron oled location on thumbnail */
 //   draw_rect_filled_soft(MATRIX_DISPLAY_X + 14, MATRIX_DISPLAY_Y + 2, 3, 1, PIXEL_ON, NORM);
 
 }
+
 
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
@@ -215,6 +207,7 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 void oled_task_user(void) {
     if (is_keyboard_master()) {
         print_status_narrow();
+        print_matrix();
     } else {
         // render_anim_cat();
         render_logo();
@@ -385,170 +378,20 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             return false;
             break;
-        case KC_A:
-            if (record->event.pressed) {
-                current_alpha_oled = 'A';
-            } else {
+        case KC_HOT_REL:
+            if (record->event.pressed){
+                tap_code16(LCTL(S(KC_R)));
+            }else{
+
             }
-            break;
-        case KC_B:
-            if (record->event.pressed) {
-                current_alpha_oled = 'B';
-            } else {
-            }
-            break;
-        case KC_C:
-            if (record->event.pressed) {
-                current_alpha_oled = 'C';
-            } else {
-            }
-            break;
-        case KC_D:
-            if (record->event.pressed) {
-                current_alpha_oled = 'D';
-            } else {
-            }
-            break;
-        case KC_E:
-            if (record->event.pressed) {
-                current_alpha_oled = 'E';
-            } else {
-            }
-            break;
-        case KC_F:
-            if (record->event.pressed) {
-                current_alpha_oled = 'F';
-            } else {
-            }
-            break;
-        case KC_G:
-            if (record->event.pressed) {
-                current_alpha_oled = 'G';
-            } else {
-            }
-            break;
-        case KC_H:
-            if (record->event.pressed) {
-                current_alpha_oled = 'H';
-            } else {
-            }
-            break;
-        case KC_I:
-            if (record->event.pressed) {
-                current_alpha_oled = 'I';
-            } else {
-            }
-            break;
-        case KC_J:
-            if (record->event.pressed) {
-                current_alpha_oled = 'J';
-            } else {
-            }
-            break;
-        case KC_K:
-            if (record->event.pressed) {
-                current_alpha_oled = 'K';
-            } else {
-            }
-            break;
-        case KC_L:
-            if (record->event.pressed) {
-                current_alpha_oled = 'L';
-            } else {
-            }
-            break;
-        case KC_M:
-            if (record->event.pressed) {
-                current_alpha_oled = 'M';
-            } else {
-            }
-            break;
-        case KC_N:
-            if (record->event.pressed) {
-                current_alpha_oled = 'N';
-            } else {
-            }
-            break;
-        case KC_O:
-            if (record->event.pressed) {
-                current_alpha_oled = 'O';
-            } else {
-            }
-            break;
-        case KC_P:
-            if (record->event.pressed) {
-                current_alpha_oled = 'P';
-            } else {
-            }
-            break;
-        case KC_Q:
-            if (record->event.pressed) {
-                current_alpha_oled = 'Q';
-            } else {
-            }
-            break;
-        case KC_R:
-            if (record->event.pressed) {
-                current_alpha_oled = 'R';
-            } else {
-            }
-            break;
-        case KC_S:
-            if (record->event.pressed) {
-                current_alpha_oled = 'S';
-            } else {
-            }
-            break;
-        case KC_T:
-            if (record->event.pressed) {
-                current_alpha_oled = 'T';
-            } else {
-            }
-            break;
-        case KC_U:
-            if (record->event.pressed) {
-                current_alpha_oled = 'U';
-            } else {
-            }
-            break;
-        case KC_V:
-            if (record->event.pressed) {
-                current_alpha_oled = 'V';
-            } else {
-            }
-            break;
-        case KC_W:
-            if (record->event.pressed) {
-                current_alpha_oled = 'W';
-            } else {
-            }
-            break;
-        case KC_X:
-            if (record->event.pressed) {
-                current_alpha_oled = 'X';
-            } else {
-            }
-            break;
-        case KC_Y:
-            if (record->event.pressed) {
-                current_alpha_oled = 'Y';
-            } else {
-            }
-            break;
-        case KC_Z:
-            if (record->event.pressed) {
-                current_alpha_oled = 'Z';
-            } else {
-            }
-            break;
+            return false;
 
     }
     return true;
 }
 
 #ifdef ENCODER_ENABLE
-bool is_alt_tab_active = false;
-uint16_t alt_tab_timer = 0;
+
 void encoder_update_user(uint8_t index, _Bool clockwise) {
     if (index == 0) {
         if (clockwise) {
@@ -558,28 +401,10 @@ void encoder_update_user(uint8_t index, _Bool clockwise) {
         }
     } else if (index == 1) {
         if (clockwise) {
-            if (!is_alt_tab_active) {
-                is_alt_tab_active = true;
-                register_code(KC_LALT);
-            }
-            alt_tab_timer = timer_read();
-            tap_code16(KC_TAB);
-
-
+            tap_code(KC_PGUP);
             } else {
-            alt_tab_timer = timer_read();
-            tap_code16(S(KC_TAB));
-            }
+            tap_code(KC_PGDN);
             }
         }
-
-        void matrix_scan_user(void) {
-            if (is_alt_tab_active) {
-                if (timer_elapsed(alt_tab_timer) > 650) {
-                unregister_code(KC_LALT);
-                is_alt_tab_active = false;
-                }
-            }
-        }
-
+}
 #endif
